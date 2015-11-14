fb82ee0d3764bd96016db713ff453c822cce5cdb
Md. Jakaria <jakaria.dcc11@gmail.com>
2015-11-15 03:14:14 +0600

    Practice

---
+++ src/sample2/sample2.java
@@ -0,0 +1,18 @@
+package sample2;/**
+ * Created by MD on 15/11/2015.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class sample2 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
